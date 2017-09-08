b4f48134a57a1aabe06892cf6618ec3bcd44bf79
Antonio Melendez <antoniomelendez@Antonios-MacBook-Pro.local>
2017-09-08 15:49:58 -0600

    Image I completed

--- mobile1/App.js
+++ mobile1/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, FlatList } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.header}> 
-          Header
-        </Text>
-        <View style={styles.image}>
-          image
+        <Text style={styles.header}>Header</Text>
+        <Image style={styles.image} source={{uri:"https://unsplash.it/400/?image=1065"}}></Image>
+        <View style={styles.buttonContainer}>
+          <Button title='Like'/>
+          <Button title='Share'/>
+          <Button title='Comment'/>
         </View>
-
-        <View style={styles.button-container}>
-          buttons
-        </View>
-        
         <View style={styles.comment}>
-          comments
+        <FlatList
+          data={[
+            {key: 'Some User:',
+            comment: 'A comment on the picture'
+            },
+            {key: 'Another User:',
+            comment: 'A comment on the picture'
+            },
+            {key: 'And Another User:',
+            comment: 'A comment on the picture'
+            },
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key} {item.comment}</Text>}
+        />
         </View>
-
       </View>
     );
   }
@@ -28,11 +36,39 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#FFFFFF',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
   header: {
-
+    marginTop: 50,
+    width: 415,
+    height: 50,
+    textAlign: 'center',
+    fontSize: 24
+  },
+  image: {
+    width: 415,
+    height: 415
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    height: 25,
+    width: 300,
+  },
+  comment: {
+    flex: 1,
+    width: 400,
+    height: 50,
+    fontSize: 20,
+    justifyContent: 'space-around'
+  },
+  item: {
+    fontSize: 16,
+    justifyContent: 'space-between',
+    borderWidth: 1
   }
 });
